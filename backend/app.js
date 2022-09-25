1c5b7b628c6150e6fb40273dbcbdea3d3af61817
adarsh25t <adarsh25t@gmail.com>
2022-09-25 10:38:25 +0530

    complete error handler

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const errorMiddleware = require('./middleware/error')
-
+const ErrorHandler = require('./utils/errorHandler');
 
 const app = express();
 app.use(express.json())
@@ -8,10 +8,17 @@ app.use(express.json())
 // Route Import
 const product = require('./routes/productRoute');
 
+
 // routers
-app.use("/api/v1",product)
+app.use("/api/v1",product);
 
 // Middleware for Errors
+app.all('*',(req,res,next) => {
+    next( new ErrorHandler(`request is not working on ${req.originalUrl} url`,404))
+})
+
 app.use(errorMiddleware);
+
+ 
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,87 +3,87 @@ const ErrorHandler = require('../utils/errorHandler')
 
 
 // create product
-exports.createProduct = async (req,res) => {
+exports.createProduct = async (req,res,next) => {
+    try {
+        const product = await Product.create(req.body)
 
-    const product = await Product.create(req.body)
-
-    res.status(201).json({
-        success:true,
-        product
-    })
+        res.status(201).json({
+            success:true,
+            product
+        })
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+    }
 }
 
 // update product by id
-exports.updateProduct = async (req,res) => {
-
-    const product = await Product.findById(req.params.id)
-
-    if (!product) {
-        res.status(500).json({
-            success:false,
-            message:"Product not found !"
+exports.updateProduct = async (req,res,next) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true,
+            useFindAndModify:false
         })
+
+        res.status(200).json({
+            success:true,
+            product
+        })
+        
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
     }
-
-    const upproduct = await Product.findByIdAndUpdate(req.params.id,req.body,{
-        new:true,
-        runValidators:true,
-        useFindAndModify:false
-    })
-
-    res.status(200).json({
-        success:true,
-        upproduct
-    })
 }
 
 // delete product by id
-exports.deleteProduct = async (req,res) => {
-    
-    let product = await Product.findById(req.params.id)
+exports.deleteProduct = async (req,res,next) => {
 
-    if (!product) {
-        res.status(500).json({
-            success:false,
-            message:"Product not found !"
+    try {
+
+        let product = await Product.findById(req.params.id)
+        await product.remove()
+
+        res.status(200).json({
+            success:true,
+            message:"Product delete successfully"
         })
+        
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
     }
-
-    await Product.remove()
-
-    res.status(200).json({
-        success:true,
-        message:"Product delete successfully"
-    })
   
 }  
 
 // get product details
 exports.getProductDetails = async (req,res,next) => {
-    let product = await Product.findById(req.params.id);
 
-    if (!product) {
+    try {
+        let product = await Product.findById(req.params.id);
+
+        res.status(200).json({   
+            success:true,
+            product
+        })
+        
+    } catch (error) {
         return next(new ErrorHandler("Product not found", 404));
     }
 
-    res.status(200).json({   
-        success:true,
-        product
-    })   
 }  
 
 // get all products 
 exports.getAllProducts = async (req,res,next) => {
 
-    const  products = await Product.find();
+    try {
+        const  products = await Product.find();
 
-    if (!products) {
+        res.status(200).json({
+            success:true,
+            products
+        })
+        
+    } catch (error) {
         return next(new ErrorHandler("Products not found !",500))
     }
-
-    res.status(200).json({
-        success:true,
-        products
-    })
 }
 
